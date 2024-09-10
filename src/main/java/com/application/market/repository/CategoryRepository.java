ae1f7f3b27dda9af8efe4c9ba16641fdb7b30388
Dumitras <dumitrufrimu118@gmail.com>
2024-09-10 11:44:05 +0300

    Code review la adaugare product si perfectionarea codului

--- src/main/java/com/application/market/repository/CategoryRepository.java
+++ src/main/java/com/application/market/repository/CategoryRepository.java
@@ -6,4 +6,6 @@ import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByCategoryName(String categoryName);
+
+    boolean existsByCategoryName(String categoryName);
 }
